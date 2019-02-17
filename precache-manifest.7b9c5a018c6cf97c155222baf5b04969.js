5e4406f4b78a3a5e82074a2f4b29f16b56094c84
gtfarng <5735512002@psu.ac.th>
2019-02-17 23:43:05 +0700

    Updates

---
+++ precache-manifest.7b9c5a018c6cf97c155222baf5b04969.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b12a95b90be5fcbf0c6e",
+    "url": "/react-app/hello-world/static/css/main.07139c8c.chunk.css"
+  },
+  {
+    "revision": "b12a95b90be5fcbf0c6e",
+    "url": "/react-app/hello-world/static/js/main.b12a95b9.chunk.js"
+  },
+  {
+    "revision": "e3cec2427d00272b6940",
+    "url": "/react-app/hello-world/static/js/runtime~main.e3cec242.js"
+  },
+  {
+    "revision": "ba1ca5ba3f9c1e3c91ad",
+    "url": "/react-app/hello-world/static/js/2.ba1ca5ba.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app/hello-world/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c8dfab1412e4d1be01ca714292d31482",
+    "url": "/react-app/hello-world/index.html"
+  }
+];
