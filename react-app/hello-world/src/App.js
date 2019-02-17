f91db0b612f8ff4879860fe0c766d1cb4f4267fe
gtfarng <5735512002@psu.ac.th>
2019-02-17 23:30:41 +0700

    deploy-react

---
+++ react-app/hello-world/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+            
+           
+         
+        </header>
+        <h1>Hello World!</h1>
+        <h1>react app</h1>
+
+
+      </div>
+    );
+  }
+}
+
+export default App;
